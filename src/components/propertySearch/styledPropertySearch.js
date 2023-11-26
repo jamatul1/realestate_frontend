0b557960d23b737f1210174fad9ea527aed5020d
unknown <newstartjam@gmail.com>
2023-11-26 17:14:41 +0600

    fixed ui of property search

--- src/components/propertySearch/styledPropertySearch.js
+++ src/components/propertySearch/styledPropertySearch.js
@@ -81,7 +81,7 @@ export const BottomButton = styled.button`
 export const Icon = styled.img``;
 export const Overlay = styled.div`
   height: 100vh;
-  width: 100vw;
+  width: 100%;
   position: absolute;
   top: 0;
   left: 0;
